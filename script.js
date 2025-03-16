80fa4eba78ff4b61ce28a39143ceafb971afdaf0
PureLifeDataService <purelifedataservice@gmail.com>
2025-03-16 21:14:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-btn").addEventListener("click", () => {
+    document.getElementById("side-menu").style.right = "0";
+});
+
+document.getElementById("close-menu").addEventListener("click", () => {
+    document.getElementById("side-menu").style.right = "-250px";
+});
